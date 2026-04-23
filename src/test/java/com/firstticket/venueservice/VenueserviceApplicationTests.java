0ef0d88519f8f2a2e103288970bb5b666e54c497
sweetRainShin <servdb99@gmail.com>
2026-04-23 13:52:28 +0900

    chore: 프로젝트 초기 설정, 의존성 추가 및 설정 파일 작성

---
+++ src/test/java/com/firstticket/venueservice/VenueserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.firstticket.venueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class VenueserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
